0a4f6eb61d0d17e76fbaba504b1123ade03ed62e
seraphD <1962840289@qq.com>
2023-06-20 23:41:14 -0400

    guess number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess Number!");
+    println!("input your number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Numebr: {secret_number}");
+
+    let mut guess = String::new();
+
+    loop {
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("Right!!");
+                break;
+            },
+            Ordering::Greater => println!("Big"),
+            Ordering::Less => println!("Small"),
+        }
+    }
 }
